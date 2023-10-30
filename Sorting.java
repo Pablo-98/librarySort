79f5ad8d80723524af50c8fc3dd1b797a0de94d0
Pablo Hernandez <pablo@Pablos-MacBook-Air.local>
2023-10-30 05:56:09 -0600

    editd bubble/selection

--- Sorting.java
+++ Sorting.java
@@ -3,79 +3,91 @@ public class Sorting{
     public static void bubbleSort(String words){
         // comparing each element in a list , swapping items if necessart, and repeating the pass throug as needed
         // sample code from csoer
+
+        // compareTo() hello.compareTo(hello) returns zero because they are the same. If first var
+        // if var1.compareTo(var2) > 0, var 1 is lexicographically greater thatn the other string
         for (int i =0; i< a.length; i++){
-            for (int j = 0; j < a.length -1 -i; j++){
-                if (a[j] > a[j+1]){
-                    int temp = a[j];
-                    a[j] = a[j+1];
-                    a[j+1] = temp;
+            for (int j = 0; j < words.length -1 -i; j++){
+                if (words[j].compareTo(words[j+1]) > 0) // if a[j] is lexicographically greater than a[j+1]
+                {
+                    int temp = words[j];
+                    words[j] = words[j+1];
+                    words[j+1] = temp;
                 }
             }
         }
 
     }
     public static void selectionSort(String words){
-        
-/* sample code from zybooks 
+        int smallest;
+        String temp;
 
-2
+        for (int i =0; i < words.length; i++){
 
-3
+            indexSmallest = i;
+            for (int j= i+1; i <words.length; j++){
+                if (words[j].compareTo(words[indexSmallest]) < 0)// if a[j] is lexicographically smaller than a at the smallestIndex
+                {
+                    indexSmallest = j;
 
-4
-
-5
-
-6
-
-7
-9
-3
-18
-8
-Unsorted
-Sorted
-for (i = 0; i < numbers.length - 1; ++i) {
-
-   // Find index of smallest remaining element
-   indexSmallest = i;
-   for (j = i + 1; j < numbers.length; ++j) {
-
-      if (numbers[j] < numbers[indexSmallest]) {
-         indexSmallest = j;
-      }
-   }
-
-   // Swap numbers[i] and numbers[indexSmallest]
-   temp = numbers[i];
-   numbers[i] = numbers[indexSmallest];
-   numbers[indexSmallest] = temp; */
+                }
+            }
+            temp = words[i];
+            words[i] = words[indexSmallest];
+            words[indexSmallest] = temp;
+        }
 }
     }
     public static void insertionSort(String words){
-        /* sample code from zybooks
-        for (i = 1; i < numbers.length; ++i) {
-  j = i;
-  // Insert numbers[i] into sorted part
-  // stopping once numbers[i] in correct position
-  while (j > 0 && numbers[j] < numbers[j - 1]) {
-   
-     // Swap numbers[j] and numbers[j - 1]
-     temp = numbers[j];
-     numbers[j] = numbers[j - 1];
-     numbers[j - 1] = temp;
-     --j;
-  }
-}
-        */
+  String temp; 
+  int j;
+  for (int i =0; i<words.length; i++){
+    j=1;
+    while (j >0 && words[j].compareTo(words[j-1]<0) ){
+        temp = words[j];
+        words[j] = words[j-1];
+        words[j-1] =temp;
+        j--;
     }
+    }
+    }
+
      public static void mergeSort(String words){
         
     }
+
+    private static void merge(String a)
      public static void quickSort(String words){
         
     }
      public static void stalinSort(String words){
-        
+        // needs configuring, sample code from geeks
+        int j =0;
+
+        while(true){
+
+            int moved = 0;
+
+            for(inti = 0; i < (words.size -1 -j); i++){
+                if (arr[i] >arr[i+1]){
+
+                    vector <int>:: iterator index;
+                    int temp;
+                    index = words.begin() +i +1;
+                    temp = arr[i+1];
+                    words.erase(index);
+
+                    words.insert(words.begin() + moved, temp);
+                    moved++
+                }
+            }
+            j++
+            if(moved ==0){
+                break;
+            }
+        }
+        for (int i = 0; i < words.size(); i++){
+            cout << words[i] << ", ";
+        }
     }
 }
